9700c0e503115993fb617dae32381555379416d6
Darnard Winborn <darnardwinborn@outlook.com>
2018-08-04 16:05:25 -0400

    Add files via upload

---
+++ HeartRates.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetHeartRateCalc
+{
+    class HeartRates
+    {
+        private string firstName;
+        private string lastName;
+        private int yearOfBirth;
+
+        private int ageInYears;
+        private int maximumRate;
+        
+        // constructor with parameters
+        public HeartRates(string firstname, string lastname, int yearofbirth)
+        {
+            DateTime thisyear = DateTime.Today;
+            firstName = firstname;
+            lastName = lastname;
+            yearOfBirth = yearofbirth;
+        }
+
+        // firstname property
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        // last name property
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        // birth year property
+        public int YearOfBirth
+        {
+            get
+            {
+                return yearOfBirth;
+            }
+            set
+            {
+                yearOfBirth = value;
+            }
+        }
+
+        // return age in years
+        public int AgeInYears
+        {
+            get
+            {
+                DateTime todaysDate = DateTime.Today;
+                ageInYears = todaysDate.Year - YearOfBirth;
+                return ageInYears;
+            }
+        }
+
+        // return maximum heart rate
+        public int MaximumRate
+        {
+            get
+            {
+                maximumRate = 220 - ageInYears;
+                return maximumRate;
+            }
+        }
+
+        // display the minimum and maximum heart rates
+        public void CalculateMinMaxHeartRate(int maximumrate)
+        {
+                double minRate, maxRate;
+                minRate = maximumrate * .5;
+                maxRate = maximumrate * .85;
+                Console.WriteLine($"Your target heart rate is {minRate} to {maxRate}");
+        }
+        
+
+
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetHeartRateCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables to be passed into constructor when object is created
+            string fname, lname;
+            int birthyear;
+
+            // prompt for first name
+            Console.WriteLine("Enter your first name: ");
+            fname = Console.ReadLine();
+
+            // prompt for last name
+            Console.WriteLine("Enter your last name: ");
+            lname = Console.ReadLine();
+
+            // prompt for birthyear
+            Console.WriteLine("Enter your birthyear: ");
+            birthyear = int.Parse(Console.ReadLine());
+
+            // instantiate object of class heartrate
+            // pass values collect to constructor
+            HeartRates person1 = new HeartRates(fname, lname, birthyear);
+
+            // display results, name, age, max and target heart rates
+            Console.WriteLine($"Name: {person1.FirstName} {person1.LastName}");
+            Console.WriteLine($"Age: {person1.AgeInYears}");
+            Console.WriteLine($"Your maximum heart rate is {person1.MaximumRate}");
+            person1.CalculateMinMaxHeartRate(person1.MaximumRate);
+            Console.ReadLine();
+        }
+    }
+}
